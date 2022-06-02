ceb7a15f5cd3a5121d479aad2e674e03d0b39f9c
AbogeJr <junioraboge@gmail.com>
2022-06-02 04:37:40 -0700

    added betty styling and documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - prints a string to stdout
- * return - exits the program with value 0
+ * Return: 0 if program ran successfully
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
