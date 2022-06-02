ae9d91f1db4b7b8ec001d0bef651fcffeb2c0249
AbogeJr <junioraboge@gmail.com>
2022-06-02 04:17:13 -0700

    betty formatting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main() {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+/*
+ *A program that prints
+ *"Programming is like building a multilingual puzzle
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
