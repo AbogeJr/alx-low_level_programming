24e0134e43c57601b18dd71e6e07bf410b927a9d
AbogeJr <junioraboge@gmail.com>
2022-06-07 05:07:50 -0700

    fixes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+/**
+ * jack_bauer - prints 24 hours
+ */
 
 void jack_bauer(void)
 {
@@ -7,7 +10,7 @@ void jack_bauer(void)
 
 	for (h = 0; h < 24; h++)
 	{
-		for(m=0; m < 60; m++)
+		for (m = 0; m < 60; m++)
 		{
 			_putchar(h / 10 + '0');
 			_putchar(h % 10 + '0');
