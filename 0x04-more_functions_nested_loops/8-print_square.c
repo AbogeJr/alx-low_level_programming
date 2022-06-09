2104fcd9c2a668153134782ce224fb60a307b030
AbogeJr <junioraboge@gmail.com>
2022-06-09 07:03:06 -0700

    added 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,16 +2,16 @@
 
 /**
  * print_square  prints a square
- * @square: size of square
+ * @size: size of square
  */
 
 void print_square(int size)
 {
 	int i, j;
 
-	for (i = square; i > 0; i--)
+	for (i = size; i > 0; i--)
 	{
-		for (j = square; j > 0; j--)
+		for (j = size; j > 0; j--)
 		{
 			_putchar(j + '0');
 		}
