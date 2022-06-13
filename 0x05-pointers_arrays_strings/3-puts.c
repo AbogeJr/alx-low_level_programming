c7b8c6c414e180026a61d5ff95f673d16ee88ad0
AbogeJr <junioraboge@gmail.com>
2022-06-13 05:19:31 -0700

    added 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to std out
+ * @s: pointer to base index of the string
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		_putchar(*s);
+		s++;
+	}
+
+}
