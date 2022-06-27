2c0dbbe78382f2ccf2c4ea577a061e3d61029fd5
AbogeJr <junioraboge@gmail.com>
2022-06-27 15:46:13 -0700

    fixes

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr_grid = malloc(sizeof(*int) * (width * height));
+	arr_grid = malloc(sizeof(int) * (width * height));
 
 	if (arr_grid)
 	{
