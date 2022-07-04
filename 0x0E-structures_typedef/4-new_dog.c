f1e995ab688134edd244daec80fb580b7a4cd359
AbogeJr <junioraboge@gmail.com>
2022-07-04 16:08:56 -0700

    fixes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,9 +18,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 
-	d -> name = name;
-	d -> age = age;
-	d -> owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 
 	return (d);
 }
