3b4f3c4a104eb1c994ab3d721a0cfd47def9a17c
AbogeJr <junioraboge@gmail.com>
2022-07-14 21:51:39 -0700

    fix 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,16 +10,15 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *ptr = NULL;
-
-	ptr = *head;
+	list_t *new_node;
+	list_t *ptr = *head;
 	new_node = malloc(sizeof(list_t));
 
 	if (!new_node)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = strlen(new_node->str);
+	new_node->len = strlen(str);
 	new_node->next = NULL;
 
 	if (*head == NULL)
